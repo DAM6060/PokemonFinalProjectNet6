f673987a2c311063d34954f9348b744aff18f176
DAM6060 <111292385+DAM6060@users.noreply.github.com>
2024-04-11 15:02:54 +0300

    Forgot The ITeamService Lol

--- PokemonFinalProjectNet6.Core/Contracts/ITeamService.cs
+++ PokemonFinalProjectNet6.Core/Contracts/ITeamService.cs
@@ -1,16 +1,18 @@
-﻿using PokemonFinalProjectNet6.Core.Models.Pokemon;
+﻿using PokemonFinalProjectNet6.Core.Enumerations;
+using PokemonFinalProjectNet6.Core.Models.Pokemon;
 using PokemonFinalProjectNet6.Core.Models.Team;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PokemonFinalProjectNet6.Core.Contracts
 {
     public interface ITeamService
     {
         Task<int> CreateAsync(TeamFormModel model, int playerId, params PokemonFormModel[] pokemons);
+
+        Task<TeamLeaderBoardQueryModel> TeamLeaderBoardAsync(
+            string? pokemonFiltering = null,
+            TeamSorting sorting=TeamSorting.MostWins,
+            int currentPage=1,
+            int teamsPerPage=10);
 
     }
 }
