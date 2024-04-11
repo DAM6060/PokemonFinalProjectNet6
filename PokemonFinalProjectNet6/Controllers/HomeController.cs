a0ff3ffa4313d27918abc445a6fb6e192e9fb3f5
DAM6060 <111292385+DAM6060@users.noreply.github.com>
2024-04-11 20:12:41 +0300

    ErrorPages

--- PokemonFinalProjectNet6/Controllers/HomeController.cs
+++ PokemonFinalProjectNet6/Controllers/HomeController.cs
@@ -25,9 +25,26 @@ namespace PokemonFinalProjectNet6.Controllers
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
+		public IActionResult Error(int statusCode)
 		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+			if (statusCode == 400)
+			{
+				return View("Error400");
+			}
+			else if (statusCode== 404)
+			{
+				return View("Error404");
+			}
+			else if (statusCode == 401)
+			{
+                return View("Error500");
+            }
+            else if (statusCode == 500)
+			{
+                return View("Error500");
+            }
+			
+            return View();
 		}
 	}
 }
