12a964a70a32742e809136a6136536d0a6c5becb
DAM6060 <111292385+DAM6060@users.noreply.github.com>
2024-04-11 20:13:42 +0300

    Battle and loby DTOs

---
+++ PokemonFinalProjectNet6.Core/Models/Battle/BattleMoveServiceModel.cs
@@ -0,0 +1,33 @@
+﻿using PokemonFinalProjectNet6.Infrastructure.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonFinalProjectNet6.Core.Models.Battle
+{
+    public class BattleMoveServiceModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Power { get; set; }
+        public int Accuracy { get; set; }
+        public int PP { get; set; }
+        public PokemonTypeCustom Type { get; set; }
+        public int EffectChance { get; set; }
+        public string Effect { get; set; } = string.Empty;
+        public int EffectDuration { get; set; }
+        public string Ailment { get; set; } = string.Empty;
+        public int AilmentChance { get; set; }
+        public DamageClass DamageClass { get; set; }
+        public bool? IsEffectUser { get; set; }
+        public int HealAmount { get; set; }
+        public HealType HealType { get; set; }
+        public int Priority { get; set; }
+
+
+
+        
+    }
+}
---
+++ PokemonFinalProjectNet6.Core/Models/Battle/Lobby.cs
@@ -0,0 +1,17 @@
+﻿using PokemonFinalProjectNet6.Core.Models.Player;
+
+namespace PokemonFinalProjectNet6.Core.Models.Battle
+{
+    public class Lobby
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public PlayerServiceModel Player1 { get; set; } = null!;
+        public BattleTeamServiceModel TeamP1 { get; set; } = null!;
+        public PlayerServiceModel Player2 { get; set; } = null!;
+        public BattleTeamServiceModel TeamP2 { get; set; } = null!;
+        public List<string> playerMessages { get; set; } = new List<string>();
+        public List<string> battleLog { get; set; } = new List<string>();
+
+    }
+}
