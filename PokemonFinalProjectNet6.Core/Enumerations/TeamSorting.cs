ce1d36c77c604cd45b7427fba48afeb15501da5d
DAM6060 <111292385+DAM6060@users.noreply.github.com>
2024-04-11 15:01:38 +0300

    Team Sorting Enum For leaderBoards

---
+++ PokemonFinalProjectNet6.Core/Enumerations/TeamSorting.cs
@@ -0,0 +1,13 @@
+﻿namespace PokemonFinalProjectNet6.Core.Enumerations
+{
+    public enum TeamSorting
+    {
+        Newest = 0,
+        Oldest = 1,
+        MostWins = 2,
+        MostGamesPlayed = 3,
+        BiggestPostiveDifferential = 4,
+        BiggestNegativeDifferential = 5,
+
+    }
+}
