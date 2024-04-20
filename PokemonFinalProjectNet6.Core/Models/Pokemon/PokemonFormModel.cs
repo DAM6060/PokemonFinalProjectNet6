7c6c99cece518269bdf8b95626796d559a7a9b31
DAM6060 <111292385+DAM6060@users.noreply.github.com>
2024-04-20 22:35:52 +0300

    Changes to pokemon Form models

--- PokemonFinalProjectNet6.Core/Models/Pokemon/PokemonFormModel.cs
+++ PokemonFinalProjectNet6.Core/Models/Pokemon/PokemonFormModel.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualBasic;
-using PokemonFinalProjectNet6.Core.Models.Ability;
+﻿using PokemonFinalProjectNet6.Core.Models.Ability;
 using PokemonFinalProjectNet6.Core.Models.Move;
 using PokemonFinalProjectNet6.Infrastructure.Constants;
 using System.ComponentModel.DataAnnotations;
@@ -19,7 +18,7 @@ namespace PokemonFinalProjectNet6.Core.Models.Pokemon
         public int BaseHp { get; set; }
 
         [Required(ErrorMessage = RequiredErrorMessage)]
-        [Range(0, MaxEvPerStat, ErrorMessage = MaxEvStatErrorMessage)]
+        
         public int EvHp { get; set; }
 
         [Required(ErrorMessage = RequiredErrorMessage)]
@@ -28,19 +27,18 @@ namespace PokemonFinalProjectNet6.Core.Models.Pokemon
         [Required(ErrorMessage = RequiredErrorMessage)]
         public int BaseAttack { get; set; }
 
-        [Required(ErrorMessage = RequiredErrorMessage)]
-        [Range(0, MaxEvPerStat, ErrorMessage = MaxEvStatErrorMessage)]
-        public int EvAttack { get; set; }
+		[Required(ErrorMessage = RequiredErrorMessage)]
+		
+		public int EvAttack { get; set; }
 
         [Required(ErrorMessage = RequiredErrorMessage)]
         public int Attack => (int)((((double)2 * (double)BaseAttack + ((double)EvAttack / (double)4)) * (double)Level / (double)100) + (double)5);
 
         [Required(ErrorMessage = RequiredErrorMessage)]
         public int BaseDefense { get; set; }
-
-        [Required(ErrorMessage = RequiredErrorMessage)]
-        [Range(0, MaxEvPerStat, ErrorMessage = MaxEvStatErrorMessage)]
-        public int EvDefense { get; set; }
+		[Required(ErrorMessage = RequiredErrorMessage)]
+		
+		public int EvDefense { get; set; }
 
         [Required(ErrorMessage = RequiredErrorMessage)]
         public int Defense => (int)((((double)2 * (double)BaseDefense + ((double)EvDefense / (double)4)) * (double)Level / (double)100) + (double)5);
@@ -49,7 +47,7 @@ namespace PokemonFinalProjectNet6.Core.Models.Pokemon
         public int BaseSpecialAttack { get; set; }
 
         [Required(ErrorMessage = RequiredErrorMessage)]
-        [Range(0, MaxEvPerStat, ErrorMessage = MaxEvStatErrorMessage)]
+        
         public int EvSpecialAttack { get; set; }
 
         [Required(ErrorMessage = RequiredErrorMessage)]
@@ -59,7 +57,6 @@ namespace PokemonFinalProjectNet6.Core.Models.Pokemon
         public int BaseSpecialDefense { get; set; }
 
         [Required(ErrorMessage = RequiredErrorMessage)]
-        [Range(0, MaxEvPerStat, ErrorMessage = MaxEvStatErrorMessage)]
         public int EvSpecialDefense { get; set; }
 
         [Required(ErrorMessage = RequiredErrorMessage)]
@@ -69,14 +66,13 @@ namespace PokemonFinalProjectNet6.Core.Models.Pokemon
         public int BaseSpeed { get; set; }
 
         [Required(ErrorMessage = RequiredErrorMessage)]
-        [Range(0, MaxEvPerStat, ErrorMessage = MaxEvStatErrorMessage)]
         public int EvSpeed { get; set; }
 
         [Required(ErrorMessage = RequiredErrorMessage)]
         public int Speed => (int)((((double)2 * (double)BaseSpeed + ((double)EvSpeed / (double)4)) * (double)Level / (double)100) + (double)5);
 
         [Required(ErrorMessage = RequiredErrorMessage)]
-        [Range(0, MaxTotalEvPoints, ErrorMessage = MaxEvStatErrorMessage)]
+        [Range(typeof(int), MinEvPerStat, MaxTotalEvPoints, ErrorMessage = MaxEvStatErrorMessage)]
         public int TotalEvPoints => EvHp + EvAttack + EvDefense + EvSpecialAttack + EvSpecialDefense + EvSpeed;
 
         [Required(ErrorMessage = RequiredErrorMessage)]
@@ -88,20 +84,18 @@ namespace PokemonFinalProjectNet6.Core.Models.Pokemon
         [Required(ErrorMessage = RequiredErrorMessage)]
         public int AbilityId { get; set; }
 
-        public IEnumerable<PokemonAbilityServiceModel> Abilities { get; set; } =
-            new List<PokemonAbilityServiceModel>();
-
-        public IEnumerable<PokemonMovesServiceModel> MovesDb { get; set; } =
-       new List<PokemonMovesServiceModel>();
-
-        public IEnumerable<MoveServiceModel> MovesForDropDown { get; set; } =
-            new List<MoveServiceModel>();
-
-        public int TeamId { get; set; }
+        public int Move1IdForDb { get; set; }      
+        public int Move2IdForDb { get; set; }      
+        public int Move3IdForDb { get; set; }      
+        public int Move4IdForDb { get; set; }      
+        public HashSet<int> MovesIdsForDb => new HashSet<int> { Move1IdForDb, Move2IdForDb, Move3IdForDb, Move4IdForDb };
+		public int TeamId { get; set; }
 
         public int PlayerId { get; set; }
 
+		public IEnumerable<MoveServiceModel> MovesForDropDown { get; set; } = new List<MoveServiceModel>();
+		public IEnumerable<AbilityServiceModel> Abilities { get; set; } = new List<AbilityServiceModel>();
 
 
-    }
+	}
 }
--- PokemonFinalProjectNet6.Core/Models/Pokemon/PokemonServiceModel.cs
+++ PokemonFinalProjectNet6.Core/Models/Pokemon/PokemonServiceModel.cs
@@ -1,12 +1,5 @@
 ﻿using PokemonFinalProjectNet6.Core.Models.Ability;
 using PokemonFinalProjectNet6.Core.Models.Move;
-using PokemonFinalProjectNet6.Core.Models.Player;
-using PokemonFinalProjectNet6.Core.Models.Team;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PokemonFinalProjectNet6.Core.Models.Pokemon
 {
@@ -22,48 +15,33 @@ namespace PokemonFinalProjectNet6.Core.Models.Pokemon
 
         public int EvHp { get; set; }
 
-        public int HP { get; set; }
-
         public int BaseAttack { get; set; }
 
         public int EvAttack { get; set;}
-
-        public int Attack { get; set; }
 
         public int BaseSpecialAttack { get; set; }
 
         public int EvSpecialAttack { get; set; }
 
-        public int SpecialAttack { get; set; }
-
         public int BaseDefense { get; set; }
 
         public int EvDefense { get; set; }
-
-        public int Defense { get; set; }
 
         public int BaseSpecialDefense { get; set; }
 
         public int EvSpecialDefense { get; set; }
 
-        public int SpecialDefense { get; set; }
-
         public int BaseSpeed { get; set; }
 
         public int EvSpeed { get; set; }
-
-        public int Speed { get; set; }
 
         public string Type1 { get; set; } = string.Empty;
 
         public string Type2 { get; set; } = string.Empty;
 
-        public AbilityServiceModel Ability { get; set; }
+        public List<AbilityServiceModel> Abilities { get; set; } = new List<AbilityServiceModel>();
 
-        //public PlayerServiceModel Player { get; set; }
-
-        // List<MoveServiceModel> Moves { get; set; } = new List<MoveServiceModel>();
-
+        public List<MoveServiceModel> Moves { get; set; } = new List<MoveServiceModel>();
        
     }
 }
