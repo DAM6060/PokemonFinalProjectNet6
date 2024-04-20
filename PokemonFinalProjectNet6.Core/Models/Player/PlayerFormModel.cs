019ada58463dc48ee0bd624d8d482db5c3b0a275
DAM6060 <111292385+DAM6060@users.noreply.github.com>
2024-04-20 22:35:36 +0300

    Player Form model forgot for the previous commit

---
+++ PokemonFinalProjectNet6.Core/Models/Player/PlayerFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static PokemonFinalProjectNet6.Infrastructure.Constants.Constant;
+namespace PokemonFinalProjectNet6.Core.Models.Player
+{
+    public class PlayerFormModel
+    {
+        [Required(ErrorMessage =RequiredErrorMessage)]
+        [StringLength(PlayerMaxLength, MinimumLength =PlayerMinLength, ErrorMessage = LenghtMessage)]
+        [Display(Name = "Player Name")]
+        public string Name { get; set; } = null!;
+
+        
+    }
+}
