680546512c43d1dc7ffb61ec4b731fcd4c62e218
DAM6060 <111292385+DAM6060@users.noreply.github.com>
2024-04-24 17:19:24 +0300

    PokemonViewModel RemoveUSings

--- PokemonFinalProjectNet6.Core/Models/Pokemon/PokemonViewModel.cs
+++ PokemonFinalProjectNet6.Core/Models/Pokemon/PokemonViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using PokemonFinalProjectNet6.Core.Models.Ability;
 using PokemonFinalProjectNet6.Core.Models.Move;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace PokemonFinalProjectNet6.Core.Models.Pokemon
 {
